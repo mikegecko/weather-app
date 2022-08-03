991f508884f94600cd2099ba8f470677422f0e2d
Michael Guedko <mjgeko@gmail.com>
2022-08-03 14:51:12 -0400

    Add basic city search

--- main.js
+++ main.js
@@ -1,36 +1,51 @@
 //openweathermap.org API key
 const api_key = "5fb856d554082723958ef5f97eed7b3d";
-const cityName = "east durham";
+//const cityName = "east durham";
 const units = "imperial";
 
 //DOM components
-const locationSpan = document.querySelector('#location');
-const currentWeatherSpan = document.querySelector('#current-weather');
-const tempSpan = document.querySelector('#current-temp');
-const feelSpan = document.querySelector('#feels-like');
-const humiditySpan = document.querySelector('#humidity');
+const locationSpan = document.querySelector("#location");
+const currentWeatherSpan = document.querySelector("#current-weather");
+const tempSpan = document.querySelector("#current-temp");
+const feelSpan = document.querySelector("#feels-like");
+const humiditySpan = document.querySelector("#humidity");
+const searchInput = document.querySelector("#location-search");
+const searchBtn = document.querySelector("#submit-search");
 
 let data;
+searchBtn.addEventListener("click", submitSearch);
+searchInput.addEventListener("keypress", function (event) {
+  if (event.keyCode === 13) {
+    submitSearch(event);
+  }
+});
 
-async function getCurrentWeather(){
-    //Returns weather data for location by calling API
-    const response = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${api_key}&units=${units}`);
+async function getCurrentWeather(cityName) {
+  //Returns weather data for location by calling API
+  try {
+    const response = await fetch(
+      `http://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${api_key}&units=${units}`
+    );
     const weatherData = await response.json();
-    return(weatherData);
+    return weatherData;
+  } catch (error) {
+    console.log(error);
+  }
 }
-async function parseWeatherData(weatherData){
-    //Return an object with required data
-    data = await weatherData;
-    WeatherDataPost(data);
-    console.log(data);
-    console.log(data.main); //Temps
-    console.log(data.weather);// Cloud cover / weather
+async function parseWeatherData(weatherData) {
+  //Return an object with required data
+  data = await weatherData;
+  WeatherDataPost(data);
+  console.log(data);
 }
-function WeatherDataPost (data) {
-    currentWeatherSpan.textContent = data.weather[0].description;
-    locationSpan.textContent = data.name;
-    tempSpan.textContent = data.main.temp;
-    feelSpan.textContent = data.main.feels_like;
-    humiditySpan.textContent = data.main.humidity;
+function submitSearch(event) {
+  parseWeatherData(getCurrentWeather(searchInput.value));
 }
-parseWeatherData(getCurrentWeather());
+function WeatherDataPost(data) {
+  currentWeatherSpan.textContent = data.weather[0].description;
+  locationSpan.textContent = data.name;
+  tempSpan.textContent = data.main.temp;
+  feelSpan.textContent = data.main.feels_like;
+  humiditySpan.textContent = data.main.humidity;
+}
+//parseWeatherData(getCurrentWeather());
